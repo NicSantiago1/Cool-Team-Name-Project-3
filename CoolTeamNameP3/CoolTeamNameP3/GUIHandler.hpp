0d4c17f80a21842e02e091305539ca909ed1d17a
jacksonavery <58484091+jacksonavery@users.noreply.github.com>
2020-12-03 10:35:37 -0500

    more work on pie charts, modular now

--- CoolTeamNameP3/CoolTeamNameP3/GUIHandler.hpp
+++ CoolTeamNameP3/CoolTeamNameP3/GUIHandler.hpp
@@ -1,68 +1,37 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include <unordered_map>
 #define PI 3.14159265f
 
-class GUIHandler {
-	sf::RenderWindow window;
-	sf::Font fontAHG;
-	std::vector<sf::Drawable*> drawables;
-	const int WIDTH = 1280, HEIGHT = 720;
+class pieChart {
+	 float PIE_R, PIE_X, PIE_Y;
+	 int MAX_COLS = 6;
+	 std::vector<sf::Color> pieColors;
+	 std::vector<sf::ConvexShape*> slices;
+
+	 //temp map (other part in const)
+	 std::map<int, std::string, std::greater<int>> hashmap;
+	 
 
 public:
-	GUIHandler() {
-		//initialize window
-		sf::ContextSettings settings;
-		settings.antialiasingLevel = 8;
-		window.create(sf::VideoMode(WIDTH, HEIGHT), "COP3530 Project 3: Team Cool Team Name", sf::Style::Default, settings);
+	void setSize(float rad, float xpos, float ypos) {
+		PIE_R = rad;
+		PIE_X = xpos;
+		PIE_Y = ypos;
+	}
 
-		//font
-		if (!fontAHG.loadFromFile("AlteHaasGroteskBold.ttf")) {
-			std::cout << "font not loaded\n";
-		}
-
-
-		//title
-		sf::RectangleShape* titlBox = new sf::RectangleShape;
-		titlBox->setSize(sf::Vector2f(WIDTH, 100));
-		titlBox->setFillColor(sf::Color(57, 50, 50));
-		titlBox->setPosition(0, 0);
-		drawables.push_back(titlBox);
-
-		sf::Text* title = new sf::Text;
-		title->setFont(fontAHG);
-		title->setString("The Politicization of the Covid-19 Pandemic on Twitter");
-		title->setCharacterSize(40);
-		title->setFillColor(sf::Color(243, 239, 231));
-		sf::FloatRect titleRect = title->getLocalBounds();
-		title->setOrigin(titleRect.width / 2.0f, titleRect.height / 2.0f);
-		title->setPosition(WIDTH / 2, 32);
-		drawables.push_back(title);
-
-
-		//temp pie chart map, ik itll be differnet in final but just for now
-		std::map<int, std::string, std::greater<int>> hashmap;
-		hashmap.insert(std::pair<int, std::string>(10, "Trump"));
-		hashmap.insert(std::pair<int, std::string>(29, "Biden"));
-		hashmap.insert(std::pair<int, std::string>(17, "Conservative"));
-		hashmap.insert(std::pair<int, std::string>(11, "Democrat"));
-		hashmap.insert(std::pair<int, std::string>(8, "Swag Money"));
-		hashmap.insert(std::pair<int, std::string>(8, "Swag"));
-		hashmap.insert(std::pair<int, std::string>(2, "Ignored"));
-
-		//pie chart things
-		const float PIE_R = 260.0f, PIE_X = WIDTH / 2 - 330, PIE_Y = HEIGHT / 2 + 50;
-		const int MAX_COLS = 6;
-		sf::Color pieColors[MAX_COLS]; //= { sf::Color(234,12,44), sf::Color::Green, sf::Color::Magenta,
-								//		 sf::Color::Red, sf::Color::Cyan, sf::Color::Black };
+	void setColor(int _r, int _g, int _b) {
 		for (int i = 0; i < MAX_COLS; i++) {
-			pieColors[i].r = 234 - (234 / MAX_COLS)*i;
-			pieColors[i].g = 12 - (12 / MAX_COLS)*i;
-			pieColors[i].b = 44 - (44 / MAX_COLS)*i;
+			sf::Color tempC;
+			tempC.r = _r - (_r / MAX_COLS)*i;
+			tempC.g = _g - (_g / MAX_COLS)*i;
+			tempC.b = _b - (_b / MAX_COLS)*i;
+			pieColors.push_back(tempC);
 		}
-		
+	}
 
-		std::vector<sf::ConvexShape*> slices;
+	void initSlices() {
 		auto j = hashmap.begin();
 		int k = j->first;
 		int m = 0;
@@ -96,7 +65,74 @@ public:
 			//		(float)(PIE_Y - PIE_R * .7 * std::cos((2.0f * PI / 100.0f)*i))));
 			//}
 		}
-		drawables.insert(drawables.end(), slices.begin(), slices.end());
+	}
+
+	void sendDrawables(std::vector<sf::Drawable*>* to) {
+		to->insert(to->end(), slices.begin(), slices.end());
+	}
+
+	pieChart(float rad, float xpos, float ypos, int r, int g, int b, std::vector<sf::Drawable*>* to) {
+		//temp map too
+		hashmap.insert(std::pair<int, std::string>(10, "Trump"));
+		hashmap.insert(std::pair<int, std::string>(29, "Biden"));
+		hashmap.insert(std::pair<int, std::string>(17, "Conservative"));
+		hashmap.insert(std::pair<int, std::string>(11, "Democrat"));
+		hashmap.insert(std::pair<int, std::string>(8, "Swag Money"));
+		hashmap.insert(std::pair<int, std::string>(8, "Swag"));
+		hashmap.insert(std::pair<int, std::string>(2, "Ignored"));
+
+		setSize(rad, xpos, ypos);
+		setColor(r, g, b);
+		initSlices();
+		sendDrawables(to);
+	}
+
+};
+
+class GUIHandler {
+	sf::RenderWindow window;
+	sf::Font fontAHG;
+	std::vector<sf::Drawable*> drawables;
+	int state = 0; //0 for pie, 1 for bar
+	std::vector<sf::Drawable*> pieDrawables;
+
+	const int WIDTH = 1280, HEIGHT = 720;
+
+public:
+	GUIHandler() {
+		//initialize window
+		sf::ContextSettings settings;
+		settings.antialiasingLevel = 8;
+		window.create(sf::VideoMode(WIDTH, HEIGHT), "COP3530 Project 3: Team Cool Team Name", sf::Style::Default, settings);
+
+		//font
+		if (!fontAHG.loadFromFile("AlteHaasGroteskBold.ttf")) {
+			std::cout << "font not loaded\n";
+		}
+
+		//title (make class plz)
+		sf::RectangleShape* titlBox = new sf::RectangleShape;
+		titlBox->setSize(sf::Vector2f(WIDTH, 90));
+		titlBox->setFillColor(sf::Color(57, 50, 50));
+		titlBox->setPosition(0, 0);
+		drawables.push_back(titlBox);
+
+		sf::Text* title = new sf::Text;
+		title->setFont(fontAHG);
+		title->setString("The Politicization of the Covid-19 Pandemic on Twitter");
+		title->setCharacterSize(40);
+		title->setFillColor(sf::Color(243, 239, 231));
+		sf::FloatRect titleRect = title->getLocalBounds();
+		title->setOrigin(titleRect.width / 2.0f, titleRect.height / 2.0f);
+		title->setPosition(WIDTH / 2, 32);
+		drawables.push_back(title);
+
+		//pie chart things
+		pieChart leftChart(160.0f, WIDTH / 2.0f - 400.0f, HEIGHT / 2.0f - 50.0f, //size
+			59, 235, 167, &pieDrawables); //color rgb and draw group
+		pieChart midChart(160.0f, WIDTH / 2.0f, HEIGHT / 2.0f - 50.0f, 199, 197, 193, &pieDrawables);
+		pieChart rightChart(160.0f, WIDTH / 2.0f + 400.0f, HEIGHT / 2.0f - 50.0f, 234, 12, 44, &pieDrawables);
+
 	}
 
 	~GUIHandler() {
@@ -113,11 +149,18 @@ public:
 			}
 		}
 
+
 		//drawing
 		window.clear(sf::Color(243, 239, 231));
 		for (auto it : drawables) { 
 			window.draw(*it);
 		}
+		if (state == 0) {
+			for (auto it : pieDrawables) {
+				window.draw(*it);
+			}
+		}
+		
 		window.display();
 
 		return true;
